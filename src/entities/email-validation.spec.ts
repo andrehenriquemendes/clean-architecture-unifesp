d0860f778851dbdb75424e092bb71ede526f8aca
André Henrique Mendes <andre.mendes@outlook.com.br>
2020-12-12 16:36:28 -0300

    test: validate emails with domain larger than 255 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -16,12 +16,17 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeTruthy()
   })
 
-  test('should accept strings larger than 320 chars', () => {
+  test('should not accept strings larger than 320 chars', () => {
     const email = 'l'.repeat(64) + '@' + 'c'.repeat(128) + '.' + 'd'.repeat(127)
     expect(Email.validate(email)).toBeFalsy()
   })
 
-  test('should accept local part larger than 64 chars', () => {
+  test('should not accept domain part larger than 255 chars', () => {
+    const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
